441f37fdc8d9bc72d780b582c84b79c9e0645550
Shimaa Elbadry <shimaa.elbadry54@gmail.com>
2018-12-10 11:36:41 +0200

    first commit

---
+++ src/main/java/tika/test/tika_test/App.java
@@ -0,0 +1,13 @@
+package tika.test.tika_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
